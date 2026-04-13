c7f745f4c929354ae56e9a3f9a08c73bfecc614f
TristanVarner <tvarner3@charlotte.edu>
2026-04-13 05:41:20 -0400

    added gitignore and added automatic deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen">
         <Nav />
         <Routes>
+          <Route index element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
